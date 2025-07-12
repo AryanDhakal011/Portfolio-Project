ba9768c3a48900f09396cd978bae24baaa69e8e3
Aryan Dhakal <aryan@example.com>
2025-07-13 00:42:25 +0545

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import { ThemeToggle } from "../components/ThemeToggle"
+import { StarBackground } from "../components/StarBackground"
+export const Home = () => { 
+
+  return ( 
+    <div className="min-h-screen ">
+
+      {/* Theme Toggle  */}
+      <ThemeToggle />
+
+      {/* Background Effects  */}
+      <StarBackground />
+      
+      {/* Navbar */}
+
+
+      {/* Main Content  */}
+
+
+      {/* Footer */}
+    </div>
+  )
+}
